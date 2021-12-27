dd4397534a2488bea7caf9d581726b36ec907140
bieg87 <marcinbieganski@wp.pl>
2021-12-27 21:44:41 +0100

    Dodanie gitignore, usunięte niepotrzebne komentarze

--- allegro/allegro/settings.py
+++ allegro/allegro/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_anp$d6jdn7#sk9fl(_m$u&yht=m!+c2)f(_m4g3b#src%z)7y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
